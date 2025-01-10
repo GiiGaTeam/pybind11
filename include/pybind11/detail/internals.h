b5999fda9865404f8f8e12ad1c1a722b8280e313
damibran <olegevdk@gmail.com>
2025-01-10 12:52:41 +0300

    inline internals_function_record_capsule_name

--- include/pybind11/detail/internals.h
+++ include/pybind11/detail/internals.h
@@ -62,7 +62,7 @@ using ExceptionTranslator = void (*)(std::exception_ptr);
 
 PYBIND11_NAMESPACE_BEGIN(detail)
 
-constexpr const char *internals_function_record_capsule_name = "pybind11_function_record_capsule";
+constexpr inline const char *internals_function_record_capsule_name = "pybind11_function_record_capsule";
 
 // Forward declarations
 inline PyTypeObject *make_static_property_type();
